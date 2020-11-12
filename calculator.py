8f1a3e722cb4118fb767a53bf6148a3d57205f0c
FlukeHermit <zayed.humayun@g.bracu.ac.bd>
2020-11-12 11:42:48 +0600

    Added loop

--- calculator.py
+++ calculator.py
@@ -1,2 +1,10 @@
-x, y = map(int, input().split())
-print(x + y)
+while True:
+    user_input = input()
+
+    if user_input == '/exit':
+        print('Bye!')
+        break
+    elif user_input == '':
+        continue
+    else:
+        print(sum(map(int, user_input.split())))
